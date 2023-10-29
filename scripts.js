f3fc7911d280e87e8599bfd2724ae9b8cb16dfb1
kbp753 <40896247+kbp753@users.noreply.github.com>
2023-10-29 00:40:52 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,6 +9,6 @@ function loadComponent(componentId, componentUrl) {
 
 // Load header and footer on page load
 window.onload = function() {
-    loadComponent('header', 'header.html');
-    loadComponent('footer', 'footer.html');
+    loadComponent('header', 'airquality-header.html');
+    loadComponent('footer', 'airquality-footer.html');
 };
